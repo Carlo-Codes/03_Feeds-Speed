f7801e5c91bcea72488391748f954e80b9334f5d
CeeLizzle <88803350+CeeLizzle@users.noreply.github.com>
2022-07-28 22:46:16 +0100

    adding click functionality to nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 let but = document.getElementById("but");
 
+let navBar = document.getElementById("nav");
+let feedSpeedsBut = navBar.getElementsByTagName("p")[0]
+let materialsBut = navBar.getElementsByTagName("p")[1]
+
+feedSpeedsBut.addEventListener("click", function(){console.log("helloworld")});
+
+materialsBut.addEventListener("click", function(){console.log("hello material world")});
+console.log(feedSpeedsBut)
+//
 
 function handlerEvent() {
   alert('hello');
