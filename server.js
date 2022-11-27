5371fcdd697d50afc4b911ef6e2e56d5c3f3d34e
unknown <carlo.scrocca@googlemail.com>
2022-11-27 18:26:55 +0000

    adding header config to cors

--- server.js
+++ server.js
@@ -140,11 +140,28 @@ dbCon.connect((err) => {
     console.log("db Connected");
 });
 
+let corsOptionconfig = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "Access-Control-Allow-Headers" : "auth, Content-Type",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204  
+}
+
+let corsConfig = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204  
+}
+
+
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsConfig));
 
-app.options('*', cors())
+
+app.options('*', cors(corsOptionconfig))
 
 
 app.get('/toolInfo', (req, res) => {
