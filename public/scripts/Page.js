a3a07a9d39b75f4dbb8819e53f07f46b26e9eb07
CeeLizzle <88803350+CeeLizzle@users.noreply.github.com>
2022-12-15 14:18:49 +0000

    css

--- public/scripts/Page.js
+++ public/scripts/Page.js
@@ -69,7 +69,7 @@ export class Page{ //should only use classes for status' and should make a class
 
      render_content(){
       this.clearPage();
-      let intro = this.createTextElement_id("div", "intro", "// fill this with comment box or something? or instructions")
+      let intro = this.createTextElement_id("div", "intro", "// Instructions coming soon")
       this.content_html.appendChild(intro);
       window.location.hash = this.title;
       
